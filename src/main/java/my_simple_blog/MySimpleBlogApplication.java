536755f0e318a93f690f78e9c737f5609b03f976
Kirill Mikhailov <kirill.mikhailov.d@gmail.com>
2025-09-07 15:33:48 +0300

    init gradle project

---
+++ src/main/java/my_simple_blog/MySimpleBlogApplication.java
@@ -0,0 +1,13 @@
+package my_simple_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySimpleBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySimpleBlogApplication.class, args);
+	}
+
+}
